656b4ba36ca1905785d4d7fc7bfcb6a1deea25aa
ivanfsilva <ivanfs27@gmail.com>
2020-11-13 20:05:20 -0300

    Configurando o MVC programaticamente

---
+++ src/main/java/config/WebXmlConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebXmlConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup( ServletContext servletContext ) throws ServletException {
+        AnnotationConfigWebApplicationContext webContext =
+                new AnnotationConfigWebApplicationContext();
+
+        webContext.register(MvcConfig.class);
+        webContext.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic reDynamic =
+                servletContext.addServlet("dispacher",
+                        new DispatcherServlet(webContext));
+
+        reDynamic.setLoadOnStartup(1);
+        reDynamic.addMapping("/");
+    }
+}
